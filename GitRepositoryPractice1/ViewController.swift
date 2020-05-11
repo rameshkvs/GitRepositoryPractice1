7e0dadba52e500c52902fcf0e480cddf857c4c2b
kvs <karedla.ramesh@gmail.com>
2020-05-11 11:32:01 +0100

    Added Label and Button

---
+++ GitRepositoryPractice1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitRepositoryPractice1
+//
+//  Created by venkata satya ramesh on 11/05/2020.
+//  Copyright © 2020 KVS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
