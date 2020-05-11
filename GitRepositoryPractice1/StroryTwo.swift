1a7fbdff72b183a74f2946415f07088116dd364b
kvs <karedla.ramesh@gmail.com>
2020-05-11 13:21:20 +0100

    Added StoryTwo.swift file

---
+++ GitRepositoryPractice1/StroryTwo.swift
@@ -0,0 +1,11 @@
+//
+//  StroryTwo.swift
+//  GitRepositoryPractice1
+//
+//  Created by venkata satya ramesh on 11/05/2020.
+//  Copyright © 2020 KVS. All rights reserved.
+//
+
+import Foundation
+
+//Added StoryTwo.swfit file
